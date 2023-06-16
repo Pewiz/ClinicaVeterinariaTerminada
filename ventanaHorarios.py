eed3fc3933cb6b55aed6274ff649fd7bacdebe14
VictorCalbucura <111377379+VictorCalbucura@users.noreply.github.com>
2023-06-16 13:27:57 -0400

    avance

--- ventanaHorarios.py
+++ ventanaHorarios.py
@@ -13,7 +13,20 @@ class ventanaHorarios(QMainWindow):
         self.cont = cont #Contador para reconocer que tipo de reserva es (quirofano, especialista, rutinario)
         self.ventanaUi = uiVent()
         self.ventanaUi.setupUi(self)
-        
+        if self.cont == 0:
+            self.ventanaUi.label_9.setText("Horarios: Ct Rutinario.")
+        elif self.cont == 1:
+            self.ventanaUi.label_9.setText("Horarios: Quirofano.")
+        elif self.cont == 2:
+            self.ventanaUi.label_9.setText("Horarios: Esp. Neurologo.")
+        elif self.cont == 3:
+            self.ventanaUi.label_9.setText("Horarios: Esp. Reproduccion.")
+        elif self.cont == 4:
+            self.ventanaUi.label_9.setText("Horarios: Esp. Odontologia.")
+        elif self.cont == 5:
+            self.ventanaUi.label_9.setText("Horarios: Esp. Oncologo.")
+        elif self.cont == 6:
+            self.ventanaUi.label_9.setText("Horarios: Esp. Cardiologia.")
         self.actualizarFecha()
         self.actualizarHorario()
         self.ventanaUi.FechaHoy.dateChanged.connect(self.actualizarHorario)
@@ -24,15 +37,50 @@ class ventanaHorarios(QMainWindow):
         self.ventanaUi.FechaHoy.setDate(fechaHoy)
 
     def actualizarHorario(self):
+        if self.cont != 0:
+            with open("veterinarios.csv") as r:
+                read = csv.reader(r)
+                next(read)
+                self.veterinarios = []
+                if self.cont == 1:
+                    for l in read:
+                        if l[6] == "Cirujano":
+                            self.veterinarios.append(l)
+                elif self.cont == 2:
+                    for l in read:
+                        if l[6] == "Neurologia":
+                            self.veterinarios.append(l)
+                elif self.cont == 3:
+                    for l in read:
+                        if l[6] == "Reproduccion":
+                            self.veterinarios.append(l)
+                elif self.cont == 4:
+                    for l in read:
+                        if l[6] == "Odontologia":
+                            self.veterinarios.append(l)
+                elif self.cont == 5:
+                    for l in read:
+                        if l[6] == "Oncologia":
+                            self.veterinarios.append(l)
+                elif self.cont == 6:
+                    for l in read:
+                        if l[6] == "Cardiologia":
+                            self.veterinarios.append(l)
         with open('salas.csv') as r:
             read = csv.reader(r)
             next(read)
             self.horarios = []
             fecha = self.ventanaUi.FechaHoy.date()
             fechaH = fecha.toString("dd/MM/yyyy")
-            for l in read:
-                if l[4] == fechaH:
-                    self.horarios.append(l)
+            if self.cont == 0:
+                for l in read:
+                    if l[4] == fechaH:
+                        self.horarios.append(l)
+            elif self.cont != 0:
+                for l in read:
+                    for k in self.veterinarios:
+                        if l[0] == k[0] and l[4] == fechaH:
+                            self.horarios.append(l)
         
         self.horarios = sorted(self.horarios, key=lambda x: (int(x[3].split(':')[0]),int(x[2])))
         
@@ -73,6 +121,6 @@ class ventanaHorarios(QMainWindow):
         
 if __name__=="__main__":
     app = QApplication(sys.argv)
-    ventanaP = ventanaHorarios(1)
+    ventanaP = ventanaHorarios(0)
     ventanaP.show()
     app.exec_()
