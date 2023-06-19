d39ec56fe85993da63f9d746eb2b51c0b75a251d
VictorCalbucura <111377379+VictorCalbucura@users.noreply.github.com>
2023-06-18 21:16:17 -0400

    listaReserva

--- ventanaListaReserva.py
+++ ventanaListaReserva.py
@@ -8,15 +8,93 @@ import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
 import PyQt5.QtCore as qc
 from uiListaReserva import Ui_mainWindow
+from ManejoArchivo import GestionArchivo
 
 class ventanaListaReserva(QMainWindow):
     def __init__(self, cont):
         super().__init__()
         self.cont = cont
+        self.resEnLista = []
         self.ui = Ui_mainWindow()
         self.ui.setupUi(self)
         self.actualizar()
         self.ui.buscarNombre.textChanged.connect(self.buscador)
+        self.ui.listaReservas.itemSelectionChanged.connect(self.seleccFila)
+        self.ui.btnEliminar.clicked.connect(self.eliminarHora)
+        
+    def seleccFila(self):
+        self.fila = self.ui.listaReservas.selectedIndexes()
+        if self.fila:
+            self.ui.btnVer.setEnabled(True)
+            self.filaSelecc = self.ui.listaReservas.currentRow()
+            i = 0
+            for l in self.resEnLista:
+                if i == self.filaSelecc:
+                    if l[5] == "Por Cumplir":
+                        self.ui.btnEliminar.setEnabled(True)
+                        self.ui.btnModificar.setEnabled(True)
+                    else:
+                        self.ui.btnEliminar.setEnabled(False)
+                        self.ui.btnModificar.setEnabled(False)
+                i += 1
+        else:
+            self.ui.btnVer.setEnabled(False)
+        
+    def eliminarHora(self):
+        t = 0
+        for r in self.resEnLista:
+            if t == self.filaSelecc:
+                res = r
+                break
+            t += 1
+        with open('salas.csv') as file:
+                reader = csv.reader(file)
+                next(reader)
+                i = 0
+                for l in reader:
+                    if l[4] == res[2] and l[3] == res[4] and l[2] == res[3]:
+                        posFila = i
+                        break
+                    i += 1
+        GestionArchivo.modificarLinea("salas.csv", posFila, 1, "None")
+        GestionArchivo.modificarLinea("salas.csv", posFila, 5, "False")
+
+        if res[1] == "Control Rutinario":
+            with open("Control.csv") as r:
+                read = csv.reader(r)
+                next(read)
+                i = 1
+                for l in read:
+                    if l[0] == res[2] and l[1] == res[0] and l[2] == res[4] and l[3] == res[3]:
+                        pos = i
+                        break
+                    i += 1
+            GestionArchivo.eliminar("Control.csv", pos)    
+        elif res[1] == "Quirofano":
+            with open("Quirofano.csv") as r:
+                read = csv.reader(r)
+                next(read)
+                i = 1
+                for l in read:
+                    if l[0] == res[2] and l[1] == res[0] and l[2] == res[4] and l[3] == res[3]:
+                        posi = i
+                        break
+                    i += 1
+            GestionArchivo.eliminar("Quirofano.csv", posi)
+        else:
+            with open("Citas.csv") as r:
+                read = csv.reader(r)
+                next(read)
+                i = 1
+                for l in read:
+                    if l[0] == res[2] and l[1] == res[0] and l[5] == res[3] and l[4] == res[4]:
+                        posi = i
+                        break
+                    i += 1
+            GestionArchivo.eliminar("Citas.csv", posi)            
+        self.actualizar()
+        
+                    
         
     
     def actualizar(self):
@@ -43,8 +121,6 @@ class ventanaListaReserva(QMainWindow):
         self.ui.listaReservas.setRowCount(contFilas)
         
         for i, reserva in enumerate(self.reservas):
-            tipo = ""
-            nombre = ""
             with open("Control.csv") as r:
                 read = csv.reader(r)
                 next(read)
@@ -99,6 +175,9 @@ class ventanaListaReserva(QMainWindow):
             
             estado = qtw.QTableWidgetItem(est)
             self.ui.listaReservas.setItem(i, 5, estado)
+            res = [nombre, tipo, reserva[4], reserva[2], reserva[3], est]
+            self.resEnLista.append(res)
+
             
     def buscador(self, texto):
         for row in range(self.ui.listaReservas.rowCount()):
@@ -110,6 +189,6 @@ class ventanaListaReserva(QMainWindow):
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
-    ventanaP = ventanaListaReserva(3)
+    ventanaP = ventanaListaReserva(1)
     ventanaP.show()
     app.exec_()
